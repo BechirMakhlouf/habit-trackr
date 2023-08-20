b07ba51b193fe8556bfec1816ac99de5674b1cec
bechirMakhlouf <bechirM@proton.me>
2023-08-20 21:36:29 +0100

    make note interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,19 @@
+interface Note {
+  description: string;
+  lastEdited: Date;
+  creationDate: Date;
+}
+
+const Notes = new Map<string, Note>();
 
 export default function Home() {
-  return <h1>Habit Trackr</h1>;
+  return (
+    <>
+      <h1>Habit Trackr</h1>
+      your Tasks:
+      {
+        Array.from(Notes).map(([_, note]) => note.description)
+      }
+    </>
+  );
 }

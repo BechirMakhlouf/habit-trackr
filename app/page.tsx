dc037789c0def0c590e662683f9ddd60f9b1abeb
bechirMakhlouf <bechirM@proton.me>
2023-08-25 21:12:28 +0100

    remove dummy tasks

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,37 @@
 // import { randomUUID } from "crypto";
 
-import { randomUUID } from "crypto";
+import { randomUUID, UUID } from "crypto";
 
 class Note {
   id: string = randomUUID();
   noteContent: string;
 
   constructor(noteContent: string = "") {
-    this.noteContent = noteContent
+    this.noteContent = noteContent;
   }
 }
 
-const Notes = new Map<string, Note>()
+// class notesState {
+//   Notes: Map<string, Note>
+//
+//   constructor(Notes) {
+//
+//   }
+// }
 
-Notes.set("hello", new Note("world"))
+const Notes = new Map<string, Note>();
 
-console.log(JSON.stringify(Object.fromEntries(Notes)))
+interface noteColumn {
+  title: string,
+  notesID: UUID[]
+}
+
+const notesState = {
+  noteColumns: [],
+};
+
+Notes.set("hello", new Note("world"));
+
 export default function Home() {
   return (
     <>
@@ -24,17 +40,9 @@ export default function Home() {
         <section className="max-w-2xl text-center border dark:border-white">
           <h2>Task Group</h2>
           <hr />
-          DragDro 
+          DragDro
           <ul>
-            
             <li className="border rounded m-2">task 1</li>
-            <li className="border rounded m-2">task 2</li>
-            <li className="border rounded m-2">task 3</li>
-            <li className="border rounded m-2">task 4</li>
-            <li className="border rounded m-2">task 5</li>
-            <li className="border rounded m-2">task 6</li>
-            <li className="border rounded m-2">task 7</li>
-            <li className="border rounded m-2">task 8</li>
           </ul>
         </section>
       </div>
